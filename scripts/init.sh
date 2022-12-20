a1569406dc10c3f069d54ce08659247bb4170d2d
muhammadwajdan <muhammad.wajdan@gmail.com>
2022-12-20 16:07:54 +0500

    (feat): server build scripts

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-ssh root@192.34.60.197
+# value= `cat $PWD/scripts/nginx.conf`
+# echo "$value"
+value=`cat $PWD/scripts/nginx.conf`
+echo "$value"
+ssh -t root@192.34.60.197 "source ~/.nvm/nvm.sh;
+git clone https://github.com/munirdev369/practice-sql.git;
+cd practice-sql;
+yarn;
+tsc && vite build;
+"
